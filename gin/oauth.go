5250d2a709d395266305a96ff635e32fa46721d1
M. Sonntag <michael.p.sonntag@gmail.com>
2016-09-20 11:05:54 +0200

    add json structs

---
+++ gin/oauth.go
@@ -0,0 +1,30 @@
+// Copyright (c) 2016, German Neuroinformatics Node (G-Node)
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted under the terms of the BSD License. See
+// LICENSE file in the root of the Project.
+
+package gin
+
+import "time"
+
+// TokenResponse Token request response json struct
+type TokenResponse struct {
+	TokenType    string  `json:"token_type"`
+	Scope        string  `json:"scope"`
+	AccessToken  string  `json:"access_token"`
+	RefreshToken *string `json:"refresh_token"`
+}
+
+// TokenInfo Token information json struct
+type TokenInfo struct {
+	URL        string    `json:"url"`
+	JTI        string    `json:"jti"`
+	EXP        time.Time `json:"exp"`
+	ISS        string    `json:"iss"`
+	Login      string    `json:"login"`
+	AccountURL string    `json:"account_url"`
+	Scope      string    `json:"scope"`
+}
